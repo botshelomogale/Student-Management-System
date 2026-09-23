9e3da3e5ecf2a37a05c61879403409bc23e7b0c6
Botshelo Mogale <bomogjhb025@student.wethinkcode.co.za>
2026-09-23 17:44:40 +0200

    Add student creation and listing

--- src/main/java/com/botshelo/student_management_system/controller/StudentController.java
+++ src/main/java/com/botshelo/student_management_system/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.botshelo.student_management_system.controller;
 
 import com.botshelo.student_management_system.model.Student;
+import org.springframework.web.bind.annotation.PostMapping;
 import com.botshelo.student_management_system.repository.StudentRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
@@ -25,4 +26,33 @@ public class StudentController {
 
         return "student-profile";
     }
+    @GetMapping("/admin/students/add")
+    public String showAddStudentForm() {
+        return "add-student";
+    }
+    @GetMapping("/admin/students")
+    public String listStudents(Model model) {
+
+        model.addAttribute("students", studentRepository.findAll());
+
+        return "student-list";
+}
+
+    @PostMapping("/admin/students")
+    public String addStudent(
+            String name,
+            String surname,
+            String email) {
+
+        Student student = new Student(
+                null,
+                name,
+                surname,
+                email
+        );
+
+        studentRepository.save(student);
+
+        return "redirect:/admin/students/add";
+}
 }
